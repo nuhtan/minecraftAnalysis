0f82c782a55647e86d78aa080faec30edb911ca6
nuhtan <ncrichardson15@gmail.com>
2021-08-05 20:24:43 -0400

    added rust conversion

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,159 @@
-use std::{fs::read_dir, process::Command, thread::{self, JoinHandle}};
+mod mining;
+mod techniques;
+
+use mvp_anvil::region::Region;
+use techniques::Technique;
+
+use std::{collections::hash_map::HashMap, env, fs::{self, File}, io::{BufRead, BufReader}};
+
+use crate::mining::iterable_ore_expansion;
 
 fn main() {
-    let mut handles: Vec<JoinHandle<()>> = Vec::new();
-    for file in read_dir("regions").unwrap() {
-        let file = file.unwrap();
-        handles.push(
-            thread::spawn(move || {
-                let mut command = Command::new("python");
-                let args = command.args(&["program.py", file.file_name().to_str().unwrap(), "basic"]);
-                let mut child = args.spawn().expect("Failed to start python program for basic");
-                child.wait().unwrap();
-                let mut command = Command::new("python");
-                let args = command.args(&["program.py", file.file_name().to_str().unwrap(), "poke"]);
-                let mut child = args.spawn().expect("Failed to start python program for poke");
-                child.wait().unwrap();
-            })
-        );
+    // let args = env::args();
+    // if args.len() > 4 {
+    //     println!("Please make sure that you provide proper arguments.")
+    // } else if args.len() == 1 {
+    //     println!("Help should be here...");
+    // } else {
+    //     println!("{}", args.len());
+    // }
+    simulate_range(String::from("r.0.0.mca"), Technique::Branch, 30, 20);
+}
+
+fn simulate_range(region_file_name: String, technique: Technique, max: i32, min: i32) {
+    let f_name = region_file_name.clone();
+    let t = technique.clone();
+    let t1 = technique.clone();
+    let t2 = technique.clone();
+    fs::remove_file(format!(
+        "mining_data/result-{}-{}.csv",
+        region_file_name,
+        t1.name()
+    )).ok();
+    File::create(format!(
+        "mining_data/result-{}-{}.csv",
+        region_file_name,
+        t2.name()
+    )).unwrap();
+    let mut csv_writer = csv::Writer::from_path(format!(
+        "mining_data/result-{}-{}.csv",
+        region_file_name,
+        technique.name()
+    ))
+    .unwrap();
+    csv_writer
+        .write_record(&[
+            "y",
+            "blocks mined",
+            "blocks exposed",
+            "lava",
+            "coal",
+            "copper",
+            "iron",
+            "lapis",
+            "redstone",
+            "gold",
+            "emeralds",
+            "diamonds",
+        ])
+        .unwrap();
+    for y in min..max {
+        let file_name = f_name.clone();
+        let tech = t.clone();
+        let results = simulate(file_name, tech, y);
+        csv_writer
+            .write_record(&[
+                y.to_string(),
+                results.get("blocks mined").unwrap().to_string(),
+                results.get("blocks exposed").unwrap().to_string(),
+                results.get("lava").unwrap().to_string(),
+                results.get("coal").unwrap().to_string(),
+                results.get("copper").unwrap().to_string(),
+                results.get("iron").unwrap().to_string(),
+                results.get("lapis").unwrap().to_string(),
+                results.get("redstone").unwrap().to_string(),
+                results.get("gold").unwrap().to_string(),
+                results.get("emeralds").unwrap().to_string(),
+                results.get("diamonds").unwrap().to_string(),
+            ])
+            .unwrap();
     }
-    for handle in handles {
-        handle.join().unwrap();
+}
+
+fn simulate(region_file_name: String, technique: Technique, y: i32) -> HashMap<String, i32> {
+    let region = Region::from_file(format!("regions/{}", region_file_name));
+    let exp_region = region.clone();
+    let sim_results = match technique {
+        Technique::Branch => techniques::branch_mining(
+            region,
+            mining::Direction::South,
+            (255, y, 255),
+            16,
+            160,
+            5
+        ),
+        Technique::BranchWithPoke => techniques::branch_mining_with_poke_holes(
+            region,
+            mining::Direction::South,
+            (255, y, 255),
+            10,
+            25,
+            5,
+            12,
+        ),
+    };
+    let mut lava = Vec::new();
+    let mut ores = Vec::new();
+    let valid = get_valid_blocks();
+    let exp_valid = valid.clone();
+    for block in sim_results.0 {
+        if block.block == "lava" || block.block == "flowing_lava" {
+            lava.push(block);
+        } else if valid.contains_key(&block.block) {
+            ores.push(block);
+        }
     }
+    let mut expanded_ores = Vec::new();
+    for ore in ores {
+        let region = exp_region.clone();
+        let valid = exp_valid.clone();
+        let mut expanded = iterable_ore_expansion(region, valid, ore.get_coords());
+        expanded_ores.append(&mut expanded);
+    }
+    let mut trimmed = Vec::new();
+    for ore in expanded_ores {
+        let mut found = false;
+        for comparison in &trimmed {
+            if ore == *comparison {
+                found = true;
+            }
+        }
+        if !found {
+            trimmed.push(ore);
+        }
+    }
+
+    let mut results = HashMap::new();
+
+    for ore in trimmed {
+        if results.contains_key(&ore.block) {
+            *results.get_mut(&ore.block).unwrap() += 1;
+        } else {
+            results.insert(ore.block, 1);
+        }
+    }
+
+    return results;
+}
+
+fn get_valid_blocks() -> HashMap<String, String> {
+    let mut map = HashMap::new();
+    for line in BufReader::new(File::open("ValidBlocks.txt").unwrap()).lines() {
+        let line = line.unwrap();
+        let parts: Vec<&str> = line.split(":").collect();
+        let input = String::from(parts[0]);
+        let output = String::from(parts[1]);
+        map.insert(input, output).unwrap();
+    }
+    return map;
 }
---
+++ src/mining.rs
@@ -0,0 +1,264 @@
+use core::panic;
+use std::collections::{HashMap, VecDeque};
+
+use mvp_anvil::region::Region;
+
+#[derive(Clone, PartialEq)]
+pub enum Direction {
+    North,
+    South,
+    East,
+    West,
+}
+
+#[derive(Clone, Debug)]
+pub struct SimpleBlock {
+    pub block: String,
+    pub x: i32,
+    pub y: i32,
+    pub z: i32,
+}
+
+impl PartialEq for SimpleBlock {
+    fn eq(&self, other: &Self) -> bool {
+        if self.x == other.x && self.y == other.y && self.z == other.z {
+            return true;
+        }
+        return false;
+    }
+}
+
+impl SimpleBlock {
+    pub fn new(coords: (i32, i32, i32), block: String) -> SimpleBlock {
+        return SimpleBlock {
+            block,
+            x: coords.0,
+            y: coords.1,
+            z: coords.2,
+        };
+    }
+
+    pub fn get_coords(self) -> (i32, i32, i32) {
+        return (self.x, self.y, self.z);
+    }
+}
+
+pub fn shift_coords(
+    direction: Direction,
+    mut coords: (i32, i32, i32),
+    amount: i32,
+) -> (i32, i32, i32) {
+    match direction {
+        Direction::North => coords.2 -= amount,
+        Direction::South => coords.2 += amount,
+        Direction::East => coords.0 += amount,
+        Direction::West => coords.0 -= amount,
+    };
+    return coords;
+}
+
+pub fn iterable_ore_expansion(
+    region: Region,
+    valid: HashMap<String, String>,
+    coords: (i32, i32, i32),
+) -> Vec<SimpleBlock> {
+    let mut deq = VecDeque::new();
+    let loop_reg = region.clone();
+    deq.push_back(SimpleBlock::new(coords, get_block(region, coords)));
+    let mut expanded = Vec::new();
+
+    while deq.len() > 0 {
+        let mut deq = deq.clone();
+        let reg = loop_reg.clone();
+        let block = deq.pop_front().unwrap();
+        let cen_block = block.clone();
+        &expanded.push(block);
+        for x in -1..2 {
+            for y in -1..2 {
+                for z in -1..2 {
+                    if !(x == 0 && y == 0 && z == 0) {
+                        let new_coords = (cen_block.x + x, cen_block.y + y, cen_block.z + z);
+                        let adj_reg = reg.clone();
+                        let adj = SimpleBlock::new(new_coords, get_block(adj_reg, new_coords));
+                        if valid.contains_key(&cen_block.block) {
+                            let mut found = false;
+                            let exp = expanded.clone();
+                            for b in exp {
+                                if b == adj {
+                                    found = true;
+                                }
+                            }
+                            for b in &deq {
+                                if *b == adj {
+                                    found = true;
+                                }
+                            }
+                            if !found {
+                                deq.push_back(adj);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return expanded;
+}
+
+pub fn get_block(region: Region, coords: (i32, i32, i32)) -> String {
+    let chunk_x = coords.0 / 16;
+    let chunk_z = coords.2 / 16;
+    let chunk = region.get_chunk(chunk_x as u32, chunk_z as u32);
+    return chunk.get_block(coords.0 % 16, coords.1, coords.2 % 16).id;
+}
+
+fn twoByOneSingle(region: Region, direction: Direction, coords: (i32, i32, i32)) -> (Vec<SimpleBlock>, u32, u32) {
+    let mut blocks = Vec::new();
+    let mut x_range = 0..1;
+    let mut z_range = 0..1;
+    match direction {
+        Direction::North | Direction::South => x_range = -1..2,
+        Direction::East | Direction::West => z_range = -1..2,
+    };
+    for x in x_range {
+        for y in 0..2 {
+            let z_range = z_range.clone();
+            for z in z_range {
+                let region = region.clone();
+                let new_coords = (coords.0 + x, coords.1 + y, coords.2 + z);
+                blocks.push(SimpleBlock::new(new_coords, get_block(region, new_coords)));
+            }
+        }
+    }
+    let y_clone = region.clone();
+    blocks.push(SimpleBlock::new((coords.0, coords.1 - 1, coords.2), get_block(region, (coords.0, coords.1 - 1, coords.2))));
+    blocks.push(SimpleBlock::new((coords.0, coords.1 + 2, coords.2), get_block(y_clone, (coords.0, coords.1 + 2, coords.2))));
+    return (blocks, 2, 8);
+}
+
+pub fn twoByOneLength(region: Region, direction: Direction, coords: (i32, i32, i32), length: i32) -> (Vec<SimpleBlock>, u32, u32) {
+    let mut results = (Vec::new(), 0, 0);
+    for n in 0..length {
+        let region = region.clone();
+        let direction = direction.clone();
+        let direction_dup = direction.clone();
+        let mut res = twoByOneSingle(region, direction, shift_coords(direction_dup, coords, n));
+        results.0.append(&mut res.0);
+        results.1 += res.1;
+        results.2 += res.2;
+    }
+    return results;
+}
+
+pub fn twoByOneEnd(region: Region, direction: Direction, coords: (i32, i32, i32)) -> (Vec<SimpleBlock>, u32, u32) {
+    let mut results = (Vec::new(), 0, 2);
+    let region_copy = region.clone();
+    match direction {
+        Direction::North => {
+            results.0.push(SimpleBlock::new((coords.0, coords.1, coords.2 - 1), get_block(region, (coords.0, coords.1, coords.2 - 1))));
+            results.0.push(SimpleBlock::new((coords.0, coords.1 + 1, coords.2 - 1), get_block(region_copy, (coords.0, coords.1 + 1, coords.2 - 1))));
+        },
+        Direction::South => {
+            results.0.push(SimpleBlock::new((coords.0, coords.1, coords.2 + 1), get_block(region, (coords.0, coords.1, coords.2 + 1))));
+            results.0.push(SimpleBlock::new((coords.0, coords.1 + 1, coords.2 + 1), get_block(region_copy, (coords.0, coords.1 + 1, coords.2 + 1))));
+        },
+        Direction::East => {
+            results.0.push(SimpleBlock::new((coords.0 + 1, coords.1, coords.2), get_block(region, (coords.0 + 1, coords.1, coords.2))));
+            results.0.push(SimpleBlock::new((coords.0 + 1, coords.1 + 1, coords.2), get_block(region_copy, (coords.0 + 1, coords.1 + 1, coords.2))));
+        },
+        Direction::West => {
+            results.0.push(SimpleBlock::new((coords.0 - 1, coords.1, coords.2), get_block(region, (coords.0 - 1, coords.1, coords.2))));
+            results.0.push(SimpleBlock::new((coords.0 - 1, coords.1 + 1, coords.2), get_block(region_copy, (coords.0 - 1, coords.1 + 1, coords.2))));
+        },
+    }
+    return results;
+}
+
+fn oneByOneSingle(region: Region, direction: Direction, coords: (i32, i32, i32)) -> (Vec<SimpleBlock>, u32, u32) {
+    let mut blocks = Vec::new();
+    let mut x_range = 0..1;
+    let mut z_range = 0..1;
+    match direction {
+        Direction::North | Direction::South => x_range = -1..2,
+        Direction::East | Direction::West => z_range = -1..2,
+    };
+    for x in x_range {
+        for y in 0..1 {
+            let z_range = z_range.clone();
+            for z in z_range {
+                let region = region.clone();
+                let new_coords = (coords.0 + x, coords.1 + y, coords.2 + z);
+                blocks.push(SimpleBlock::new(new_coords, get_block(region, new_coords)));
+            }
+        }
+    }
+    let y_clone = region.clone();
+    blocks.push(SimpleBlock::new((coords.0, coords.1 - 1, coords.2), get_block(region, (coords.0, coords.1 - 1, coords.2))));
+    blocks.push(SimpleBlock::new((coords.0, coords.1 + 1, coords.2), get_block(y_clone, (coords.0, coords.1 + 2, coords.2))));
+    return (blocks, 1, 5);
+}
+
+fn oneByOneLength(region: Region, direction: Direction, coords: (i32, i32, i32), length: i32) -> (Vec<SimpleBlock>, u32, u32) {
+    let mut results = (Vec::new(), 0, 0);
+    for n in 0..length {
+        let region = region.clone();
+        let direction = direction.clone();
+        let direction_dup = direction.clone();
+        let mut res = oneByOneSingle(region, direction, shift_coords(direction_dup, coords, n));
+        results.0.append(&mut res.0);
+        results.1 += res.1;
+        results.2 += res.2;
+    }
+    return results;
+}
+
+fn oneByOneEnd(region: Region, direction: Direction, coords: (i32, i32, i32)) -> (Vec<SimpleBlock>, u32, u32) {
+    let mut results = (Vec::new(), 0, 1);
+    match direction {
+        Direction::North => {
+            results.0.push(SimpleBlock::new((coords.0, coords.1, coords.2 - 1), get_block(region, (coords.0, coords.1, coords.2 - 1))));
+        },
+        Direction::South => {
+            results.0.push(SimpleBlock::new((coords.0, coords.1, coords.2 + 1), get_block(region, (coords.0, coords.1, coords.2 + 1))));
+        },
+        Direction::East => {
+            results.0.push(SimpleBlock::new((coords.0 + 1, coords.1, coords.2), get_block(region, (coords.0 + 1, coords.1, coords.2))));
+        },
+        Direction::West => {
+            results.0.push(SimpleBlock::new((coords.0 - 1, coords.1, coords.2), get_block(region, (coords.0 - 1, coords.1, coords.2))));
+        },
+    }
+    return results;
+}
+
+fn pokeStart(region: Region, coords: (i32, i32, i32)) -> (Vec<SimpleBlock>, u32, u32) {
+    let mut results = (Vec::new(), 0, 1);
+    results.0.push(SimpleBlock::new((coords.0, coords.1 + 1, coords.2), get_block(region, (coords.0, coords.1 + 1, coords.2))));
+    return results;
+}
+
+fn poke(region: Region, direction: Direction, coords: (i32, i32, i32), depth: i32) -> (Vec<SimpleBlock>, u32, u32) {
+    if depth < 1 {
+        panic!("Poke should be at least 1 block in depth")
+    }
+    let length_region = region.clone();
+    let length_direction2 = direction.clone();
+    let end_region = region.clone();
+    let end_direction = direction.clone();
+    let end_direction2 = direction.clone();
+    let mut results = (Vec::new(), 0, 0);
+    let mut res = pokeStart(region, coords);
+    results.0.append(&mut res.0);
+    results.1 += res.1;
+    results.2 += res.2;
+    let mut res = oneByOneLength(length_region, direction, shift_coords(length_direction2, coords, 1), depth - 1);
+    results.0.append(&mut res.0);
+    results.1 += res.1;
+    results.2 += res.2;
+    let mut res = oneByOneEnd(end_region, end_direction, shift_coords(end_direction2, coords, depth - 1));
+    results.0.append(&mut res.0);
+    results.1 += res.1;
+    results.2 += res.2;
+    return results;
+}
