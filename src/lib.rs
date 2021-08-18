bb65c697339325b6675aa1ccced6668f79a81c81
nuhtan <ncrichardson15@gmail.com>
2021-08-18 02:35:28 -0400

    added program status

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+use std::time::Instant;
+
+use techniques::Technique;
+
 pub mod mining;
 pub mod simulations;
 pub mod techniques;
+
+#[derive(Clone)]
+pub enum ProgramStatus {
+    // id, simulation_type, region_file, start_time, y
+    StartingSim(u32, Technique, String, Instant, i32),
+    // id, activity, blocks, exposed, lava, ores
+    UpdateSim(u32, String, u32, u32, u32, u32),
+    // id, end_time
+    FinishSim(u32)
+}
