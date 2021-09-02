f99b785f133d22e94c6a34a1ba07890ce522164f
nuhtan <ncrichardson15@gmail.com>
2021-09-01 22:38:54 -0400

    added region abstraction that caches chunks

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use std::{collections::VecDeque, fs, iter::FromIterator, path::Path, process, time::Instant};
 
+use mvp_anvil::{chunk::Chunk, region::Region};
 use techniques::Technique;
 use tui::widgets::ListState;
 
@@ -16,4 +17,25 @@ pub enum ProgramStatus {
     UpdateSim(u32, String, u32, u32, u32, u32),
     // id, end_time
     FinishSim(u32)
+}
+pub struct CachingRegion<'a> {
+    region: &'a Region<'a>,
+    chunks: Vec<Vec<Option<Chunk>>>,
+}
+
+impl<'a> CachingRegion<'a> {
+    fn new(region: &'a Region<'a>) -> CachingRegion<'a> {
+        return CachingRegion {
+            region,
+            chunks: vec![vec![None; 32]; 32]
+        }
+    }
+
+    fn get_chunk(&mut self, x: usize, z: usize) -> &Chunk {
+        if self.chunks[x][z].is_none() {
+            self.chunks[x][z] = Some(self.region.get_chunk(x as u32, z as u32));
+        }
+
+        return self.chunks[x][z].as_ref().unwrap();
+    }
 }
